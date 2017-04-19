d836419d2573c30411ff136e7e18371158805089
Sébastien de Melo <s.demelo@utb.fr>
2017-04-19 09:45:59 +0200

    Took holidays into account

--- japanese.py
+++ japanese.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import calendar
 import datetime
+import workalendar.core as cal
 from collections import namedtuple
 from errbot import BotPlugin, botcmd, arg_botcmd, webhook, ONLINE, AWAY
+from workalendar.europe import France
 
+CALENDAR = France()
 NOON_TIME = datetime.time(hour=12)
 
 
@@ -12,10 +14,6 @@ class Japanese(BotPlugin):
     """
     Tell when is (finally!) the next Japanese
     """
-
-    '''def __init__(self, *args, **kwargs):
-       BotPlugin.__init__(self, *args, **kwargs)
-       self._online = True'''
 
     '''def get_configuration_template(self):
         """
@@ -26,48 +24,13 @@ class Japanese(BotPlugin):
         return {'EXAMPLE_KEY_1': "Example value",
                 'EXAMPLE_KEY_2': ["Example", "Value"]
                }'''
-
-    '''def callback_message(self, message):
-        """Triggered for every received message that isn't coming from the bot itself"""
-        print('Body:', message.body)
-        print('Ctx:', message.ctx)
-        print('Delayed:', message.delayed)
-        print('Extras:', message.extras)
-        print('Flow:', message.flow)
-        print('Frm:', message.frm)
-        print('Direct:', message.is_direct)
-        print('Group:', message.is_group)
-        print('To: ', message.to)
-        print('Type:', message.type)
-        print(message)
-        #  'body', 'clone', 'ctx', 'delayed', 'extras', 'flow', 'frm', 'is_direct', 'is_group', 'to', 'type'
-        if self._online and 'carotide' in message.body:
-            self.change_presence(AWAY, 'Tu m\'as saoulé !')
-            self._online = False
-        elif not self._online and 'pardon' in message.body:
-            self.change_presence(ONLINE, 'C\'est bon pour cette fois')
-            self._online = True'''
-
-    '''def callback_presence(self, presence):
-        print(presence)
-        print(dir(presence))
-        if presence.get_message() is not None:
-            print(presence)'''
-
+    
     @classmethod
-    def delta_to_dhms(cls, delta):
-        Delta = namedtuple('Delta', ('days', 'hours', 'minutes', 'seconds'))
-        units = (
-           60 * 60,  # hours
-           60,  # minutes
-           1  # seconds
-        )
-        l = [delta.days]
-        seconds = delta.seconds
-        for unit in units:
-            l.append(seconds // unit)
-            seconds = seconds % unit
-        return Delta(*l)
+    def find_following_working_weekday(cls, day, weekday):
+        d = CALENDAR.get_first_weekday_after(day, weekday)
+        while CALENDAR.is_holiday(d):
+            d += datetime.timedelta(days=7)
+        return d
 
     # Passing split_args_with=None will cause arguments to be split on any kind
     # of whitespace, just like Python's split() does
@@ -75,14 +38,21 @@ class Japanese(BotPlugin):
     def japonais(self, message, args):
         """Return the time delta of the next Japanese"""
         now = datetime.datetime.now()
-        days_to_next_japanese = (calendar.FRIDAY - now.weekday()) % 7
-        japanese_datetime = datetime.datetime.combine((now + datetime.timedelta(days_to_next_japanese)).date(), NOON_TIME)
+        # now = datetime.datetime(2017, 4, 25, 13)
+        WEEKDAY = cal.FRI
+        japanese_datetime = datetime.datetime.combine(self.find_following_working_weekday(now, WEEKDAY), NOON_TIME)
+        if japanese_datetime < now:
+            japanese_datetime = datetime.datetime.combine(self.find_following_working_weekday(now + datetime.timedelta(days=1), WEEKDAY), NOON_TIME)
         delta = japanese_datetime - now
-        if days_to_next_japanese > 0:
-            if days_to_next_japanese == 1:
+        delta_days = (japanese_datetime.date() - now.date()).days
+        if delta_days > 0:
+            if delta_days == 1:
                 text = 'C\'est demain !'
             else:
-                text = 'Le Japonais est encore dans {} jours. :('.format(days_to_next_japanese)
+                if delta_days > 7:
+                    text = 'À cause des jours fériés, le prochain Japonais est encore dans {} jours. :( :( :('.format(delta_days)
+                else:
+                    text = 'Le Japonais est encore dans {} jours. :('.format(delta_days)
         else:
             text = 'C\'est aujourd\'hui le Japonais !\n'
             if delta.seconds > 3600:
